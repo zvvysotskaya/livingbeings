e3f3004f6cf70154028ead3b0f63d5145624ff33
zvvysotskaya <zhannavyvy@gmail.com>
2019-08-14 22:02:06 -0400

    adding route to th app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './pages/home';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+      <div className="App">
+          <Switch>
+              <Route exact path='/' component={Home}/>
+          </Switch>      
     </div>
   );
 }
